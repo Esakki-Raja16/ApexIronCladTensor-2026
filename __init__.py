4c89d3a68d633281994a6ec9dfe520fd1c323c5c
FFTOPPER <adhejebalaejie779@gmail.com>
2026-04-17 10:04:30 +0530

    Add project metadata and get_project_info function

--- __init__.py
+++ __init__.py
@@ -9,9 +9,11 @@ Later stages of the project migrated to a modular service-based
 architecture, but this file is retained for package compatibility.
 """
 
+
 __version__ = "0.1.0"
 __project__ = "MedFlow Nexus"
 __author__ = "Development Team"
+
 
 def get_project_info():
     return {
